031dce5195e0d30dad0db5715b0eae75d5438740
emily-ribeiro <emily.ribeiro@acad.ufsm.br>
2025-10-15 22:05:43 -0300

    funcao

---
+++ Funcao.js
@@ -0,0 +1,14 @@
+//Funções JavaScript
+
+function torrar () {
+    console.log("Torrando o pão")
+}
+
+function inserirPao () {
+    console.log("Inserindo o pão na torradeira")
+    console.log("Aguardando o pão torrar")
+        torrar()
+        console.log("Pão pronto, retirando da torradeira")
+}
+inserirPao()
+
