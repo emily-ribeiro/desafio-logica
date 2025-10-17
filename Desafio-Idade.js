11f6726809eb2f643b70d8d780ddf8023e522811
emily-ribeiro <emily.ribeiro@acad.ufsm.br>
2025-10-17 20:13:04 -0300

    Desafio-Idade

---
+++ Desafio-Idade.js
@@ -0,0 +1,12 @@
+let idade = 5; // Declaração de variável
+
+if (idade <= 12) {
+     console.log(`A pessoa com ${idade} anos é uma Criança`);
+} else if (idade <=17) {
+    console.log(`A pessoa com ${idade} anos é um Adolescente`);
+} else if (idade <=60) {
+    console.log(`A pessoa com ${idade} anos é um Adulto`);
+} 
+else if (idade > 60){ 
+    console.log(`A pessoa com ${idade} anos é um Idoso`);
+}
