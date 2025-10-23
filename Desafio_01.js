07870b3df4d158dea3caeecbf67c733e7afe2161
emily-ribeiro <emily.ribeiro@acad.ufsm.br>
2025-10-22 22:07:24 -0300

    git commit -m "Resolução dos desafios 01 a 06 JavaScript."

---
+++ Desafio_01.js
@@ -0,0 +1,10 @@
+// Hardcode the input values to avoid the ReferenceError
+const posicaoInicial = 5; 
+const totalPassos = 10; 
+
+// Calcule a posição final do herói:
+const posicaoFinal = posicaoInicial + totalPassos;
+
+// Imprime a posição final
+console.log("Posicao final do heroi:", posicaoFinal);
+// Output: Posicao final do heroi: 15
---
+++ Desafio_02.js
@@ -0,0 +1,8 @@
+let num1 = 45;
+let num2 = 40;
+
+//TODO: Implemente a lógica para exibir o xpGanho:
+const xpGanho = (num1 * num2) * 100;
+
+// Imprime a quantidade de XP ganho
+console.log("Voce ganhou " + xpGanho + " XP!");
